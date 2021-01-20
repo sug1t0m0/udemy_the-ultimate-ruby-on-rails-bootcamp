a7f274eb520d3fe616a33d463be9ebd7fce6c109
tomo_s <f15c017d@gmail.com>
2021-01-21 03:31:31 +0900

    モデルを作ったときtypoしてたので修正

--- app/models/qustion.rb
+++
@@ -1,2 +0,0 @@
-class Qustion < ApplicationRecord
-end
--- db/migrate/20210120182711_create_questions.rb
+++ db/migrate/20210120182711_create_questions.rb
@@ -1,4 +1,4 @@
-class CreateQustions < ActiveRecord::Migration[5.2]
+class CreateQuestions < ActiveRecord::Migration[5.2]
   def change
     create_table :questions do |t|
       t.string :name
