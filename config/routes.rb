a74a7ae1d37a65473d69f9b2a21664330c0d972b
tomo_s <f15c017d@gmail.com>
2021-01-21 03:20:55 +0900

    ルートでもquestions#editを呼ぶように

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   # get 'questions/show'
   # get 'questions/new'
   # get 'questions/edit'
+
+  root 'questions#index'
   resources :questions
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
