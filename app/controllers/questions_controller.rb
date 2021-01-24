87d7137ae8c63c8cb0dc8e0d9112b64022d9dc4c
tomo_s <f15c017d@gmail.com>
2021-01-24 16:15:30 +0900

    質問の新規作成に失敗したら questions/new を表示

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -17,6 +17,7 @@ class QuestionsController < ApplicationController
       redirect_to root_path, notice: 'Success!'
     else
       flash[:alert] = 'Save error!'
+      render :new
     end
   end
 
